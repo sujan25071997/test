cbe42512477baa4567f3ad487d2b3417f371a56f
sujan25071997 <sujan25071997@gmail.com>
2020-12-13 10:29:39 +0530

    settings file is modified acccording to developmnet and productions

---
+++ Technogeeks/settings/prod.py
@@ -0,0 +1,11 @@
+from Technogeeks.settings.base import *
+
+
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
